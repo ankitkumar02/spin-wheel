dd0cedb58522131a41d28bd96003da8849155ec7
Ankit Kumar <ankitkumar02@users.noreply.github.com>
2020-10-29 22:10:21 +0530

    pushing static files for hosting

---
+++ precache-manifest.c762ef550b04a55d5a9494a8fedc90d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f2d31f964d89777fafc6893a06ecdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d7002efd24b7443a39",
+    "url": "/static/css/main.9ac6a3df.chunk.css"
+  },
+  {
+    "revision": "305c78b5c18126d510e6",
+    "url": "/static/js/2.3900c058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3900c058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d7002efd24b7443a39",
+    "url": "/static/js/main.c3241aa9.chunk.js"
+  },
+  {
+    "revision": "46a70b26d5a37220cd43",
+    "url": "/static/js/runtime-main.d86ad9ee.js"
+  },
+  {
+    "revision": "242f81858a4f51b830d0559d5a6459f1",
+    "url": "/static/media/Error.242f8185.svg"
+  },
+  {
+    "revision": "a31ce90d52211543ff4fdfd3eb8929e6",
+    "url": "/static/media/ludo.a31ce90d.png"
+  }
+]);
